fef2c397e62e0045074f7e25a0dc0b0d13721b58
Lukas Klassen <inf4143@hs-worms.de>
2024-11-29 16:25:56 +0100

    feat: add register form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
-    { path: 'anmelden', component: LoginComponent }
+    { path: 'anmelden', component: LoginComponent },
+    { path: 'registrieren', component: RegisterComponent }
 ];
