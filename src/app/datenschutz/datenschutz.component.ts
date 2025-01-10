452e675b7d01dd807f7f6681c85b56ecca387f17
thedemuth <eric-demuth@web.de>
2025-01-10 13:15:35 +0100

    feat: add datenschutz component

---
+++ src/app/datenschutz/datenschutz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datenschutz',
+  standalone: true,
+  imports: [],
+  templateUrl: './datenschutz.component.html',
+  styleUrl: './datenschutz.component.css'
+})
+export class DatenschutzComponent {
+
+}
