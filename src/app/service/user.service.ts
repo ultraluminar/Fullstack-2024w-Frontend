ad34df71fd2296e45d91a04e054c1771a754db7e
thedemuth <eric-demuth@web.de>
2025-01-16 20:37:15 +0100

    feat: add user service

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../../../../interface/user';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getUser(userId: number):Observable<User>{
+    return this.httpClient.get<User>(`http://localhost:8080/users/${userId}`)
+  }
+  public deleteUser(userId:number):Observable<User>{
+    return this.httpClient.delete<User>(`http://localhost:8080/users/${userId}`)
+  }
+}
