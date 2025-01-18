29e7d91041cb6b838ebf1ad632bb5f09705caea8
thedemuth <eric-demuth@web.de>
2025-01-18 16:28:46 +0100

    feat: add getQuestionAnswer function to question service

--- src/app/service/question.service.ts
+++ src/app/service/question.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Question } from '../model/question/question';
 import { CreateQuestion } from '../model/question/create-question';
+import { QuestionArray } from '../model/question/question-array';
+import { AnswerArray } from '../model/answer/answer-array';
 
 @Injectable({
   providedIn: 'root'
@@ -26,7 +28,10 @@ export class QuestionService {
     return this.httpClient.delete<void>(`http://localhost:8080/questions/${questionId}`)
   }
 
-  public getAllQuestions(): Observable<Question[]> {
-    return this.httpClient.get<Question[]>(`http://localhost:8080/questions`);
+  public getAllQuestions(): Observable<QuestionArray> {
+    return this.httpClient.get<QuestionArray>(`http://localhost:8080/questions`);
+  }
+  public getQuestionAnswers(questionId: number): Observable<AnswerArray>{
+    return this.httpClient.get<AnswerArray>(`http://localhost:8080/questions/${questionId}/answer`)
   }
 }
