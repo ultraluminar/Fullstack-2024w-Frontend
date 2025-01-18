b1b580b609dbfcf92e45faea0c7617b7dbd41604
thedemuth <eric-demuth@web.de>
2025-01-18 17:43:56 +0100

    feat: add getAnswer function to answer service

--- src/app/service/answer.service.ts
+++ src/app/service/answer.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Answer } from '../model/answer/answer';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,9 @@ export class AnswerService {
 
   constructor(httpClient: HttpClient) {
     this.httpClient = httpClient;
+  }
+
+  public getAnswer(answerId: number): Observable<Answer> {
+    return this.httpClient.get<Answer>(`http://localhost:8080/questions/${answerId}`);
   }
 }
