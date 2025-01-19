b49665b7d1415d92e2791c708747e56be136d307
Adrian Lösch <loeschadrianp+git@gmail.com>
2025-01-19 11:37:32 +0100

    feat: enhance AuthService with token management and user state handling

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,26 +1,92 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { LoginUser } from '../model/user/login-user';
 import { CreateUser } from '../model/user/create-user';
 import { LoginResponse as LoginResponseInterface } from "../../../../interface/login-response";
+import { User } from '../model/user/user';
+import { decodeJwt } from 'jose';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   httpClient: HttpClient;
+  userService: UserService;
 
-  constructor(httpClient: HttpClient) {
+  private tokenKey = 'token';
+  private userKey = 'user';
+  private loggedIn = new BehaviorSubject<boolean>(this.hatToken());
+  private user = new BehaviorSubject<User | null>(this.getUser());
+
+  constructor(httpClient: HttpClient, userService: UserService) {
     this.httpClient = httpClient;
+    this.userService = userService;
+  }
+
+  private hatToken(): boolean {
+    return !!localStorage.getItem(this.tokenKey);
+  }
+
+  private getUser(): User | null {
+    const user = localStorage.getItem(this.userKey);
+    return user ? JSON.parse(user) : null;
+  }
+
+  private handleAuthentication(token: string): void {
+    const decodedToken = decodeJwt(token);
+    const userIdField = decodedToken["userId"];
+    if (!userIdField) {
+      return;
+    }
+    const userId = Number(userIdField);
+    this.userService.getUser(userId).subscribe(user => {
+      localStorage.setItem(this.tokenKey, token);
+      localStorage.setItem(this.userKey, JSON.stringify(user));
+      this.loggedIn.next(true);
+      this.user.next(user);
+    });
   }
 
   public login(loginUser: LoginUser): Observable<LoginResponseInterface>{
-    return this.httpClient.post<LoginResponseInterface>('http://localhost:8080/users/login', loginUser);
+    return this.httpClient.post<LoginResponseInterface>('http://localhost:8080/users/login', loginUser).pipe(
+      tap(response => {
+        const token = response.token;
+        if (token) {
+          this.handleAuthentication(token);
+        }
+      })
+    );
   }
 
   public register(createUser: CreateUser): Observable<LoginResponseInterface>{
-    return this.httpClient.post<LoginResponseInterface>('http://localhost:8080/users/register', createUser);
+    return this.httpClient.post<LoginResponseInterface>('http://localhost:8080/users/register', createUser).pipe(
+      tap(response => {
+        const token = response.token;
+        if (token) {
+          this.handleAuthentication(token);
+        }
+      })
+    );
   }
 
+  public logout(): void {
+    localStorage.removeItem(this.tokenKey);
+    localStorage.removeItem(this.userKey);
+    this.loggedIn.next(false);
+    this.user.next(null);
+  }
+
+  public isLoggedIn(): Observable<boolean> {
+    return this.loggedIn.asObservable();
+  }
+
+  public getUserObservable(): Observable<User | null> {
+    return this.user.asObservable();
+  }
+
+  public getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
 }
