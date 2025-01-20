dca0a988d10aba4d1a0a1c0163de17bfc4465458
Adrian Lösch <loeschadrianp+git@gmail.com>
2025-01-20 01:56:24 +0100

    feat: add authorization header to deleteAnswer method in AnswerService

--- src/app/service/answer.service.ts
+++ src/app/service/answer.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Answer } from '../model/answer/answer';
 import { UpdateAnswer } from '../model/answer/update-answer';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnswerService {
   httpClient: HttpClient;
+  authService: AuthService;
 
-  constructor(httpClient: HttpClient) {
+  constructor(httpClient: HttpClient, authService: AuthService) {
     this.httpClient = httpClient;
+    this.authService = authService;
+  }
+
+  private getHeaderWithToken(): HttpHeaders {
+    const token = this.authService.getToken();
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
   }
 
   public getAnswer(answerId: number): Observable<Answer> {
@@ -19,7 +27,8 @@ export class AnswerService {
   }
 
   public deleteAnswer(answerId: number): Observable<void> {
-    return this.httpClient.delete<void>(`http://localhost:8080/answers/${answerId}`);
+    const headers = this.getHeaderWithToken();
+    return this.httpClient.delete<void>(`http://localhost:8080/answers/${answerId}`, { headers });
   }
 
   public updateAnswer(answerId:number, updateAnswer: UpdateAnswer): Observable<Answer> {
