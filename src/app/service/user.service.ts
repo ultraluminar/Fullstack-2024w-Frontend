8b897693bb9ca67182ed4f42c8bf832aeacafa32
Adrian Lösch <loeschadrianp+git@gmail.com>
2025-01-20 03:20:00 +0100

    feat: add authorization header to deleteUser method

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,23 +1,35 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../model/user/user';
 import { Observable } from 'rxjs';
 import { QuestionArray } from '../model/question/question-array';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private httpClient: HttpClient;
+  private authService: AuthService;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(httpClient: HttpClient, authService: AuthService) {
+    this.httpClient = httpClient;
+    this.authService = authService;
+  }
+
+  private getHeaderWithToken(): HttpHeaders {
+    const token = this.authService.getToken();
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
 
   public getUser(userId: number):Observable<User>{
-    return this.httpClient.get<User>(`http://localhost:8080/users/${userId}`)
+    return this.httpClient.get<User>(`http://localhost:8080/users/${userId}`);
   }
   public deleteUser(userId:number):Observable<User>{
-    return this.httpClient.delete<User>(`http://localhost:8080/users/${userId}`)
+    const headers = this.getHeaderWithToken();
+    return this.httpClient.delete<User>(`http://localhost:8080/users/${userId}`, { headers });
   }
   public getUserQuestions(userId: number):Observable<QuestionArray>{
-    return this.httpClient.get<QuestionArray>(`http://localhost:8080/users/${userId}/questions`)
+    return this.httpClient.get<QuestionArray>(`http://localhost:8080/users/${userId}/questions`);
   }
 }
